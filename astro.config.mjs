2b4cb7b66449073ad91522ce72d69a7a19ae3af4
nattakingo <80042907+nattakingo@users.noreply.github.com>
2026-01-12 17:51:35 +0100

    Configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://nattakingo.github.io',
+  base: '/infissiparadise',
   vite: {
     plugins: [tailwindcss()],
   },
